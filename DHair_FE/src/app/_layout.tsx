79663da2609653f48489001ad646bf9e915c542e
Datdo010905 <dotiendat092005@gmail.com>
2026-09-16 21:46:46 +0700

    add tailwindcss

--- DHair_FE/src/app/_layout.tsx
+++ DHair_FE/src/app/_layout.tsx
@@ -1,6 +1,5 @@
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
 import { useColorScheme } from "react-native";
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
